c2565911704e109b601574d11bb4c739fc2cb82e
Mehrozz-Ali <mehrozali912@gmail.com>
2025-12-25 23:12:07 +0500

    Login & Sign-Up page updated

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -1,14 +1,15 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
-
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import { LoginPage, SignUpPage } from './Routes.jsx'
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
     <>
-      <h1 className='text-red-400'>Hello </h1>
+      <BrowserRouter>
+        <Routes>
+          <Route path='/login' element={<LoginPage />} />
+          <Route path='/sign-up' element={<SignUpPage />} />
+        </Routes>
+      </BrowserRouter>
     </>
   )
 }
