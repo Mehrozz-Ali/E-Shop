463b47c6262bdfc11bba34e9c9621fbfc2121330
Mehrozz-Ali <mehrozali912@gmail.com>
2025-12-26 12:35:39 +0500

    Building a mongoDB connection

--- Backend/server.js
+++ Backend/server.js
@@ -1,25 +1,30 @@
-const app=require("./app");
-
-
+const app = require("./app");
 
 
 // handling uncaught Exception
-process.on("uncaughtException",(err)=>{
-    console.log(`Error ${err.message}`); 
+process.on("uncaughtException", (err) => {
+    console.log(`Error ${err.message}`);
     console.log(`Shutting down the server for handling uncaught exception`);
 })
 
 
 // config 
-if(process.env.NODE_ENV !== "PRODUCTION"){
-    require("dotenv").config({path: "backend/config/.env"})
+if (process.env.NODE_ENV !== "PRODUCTION") {
+    require("dotenv").config({ path: "backend/config/.env" })
 }
 
 
 // create server
-const server =app.listen(process.env.PORT, ()=>{
+const server = app.listen(process.env.PORT, () => {
     console.log(`Server is running on http://localhost:${process.env.PORT}`);
 })
 
 
 // unhandled promise rejection
+process.on("unhandledRejection", (err) => {
+    console.log(`Shutting down the server for ${err.message}`);
+    console.log(`Shutting down the server for unhandled promise rejection`);
+    server.close(() => {
+        process.exit(1);
+    });
+})
