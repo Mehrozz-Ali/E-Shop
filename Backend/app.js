81de0c47bbe7d8643f856343a86d995ee6192fd2
Mehrozz-Ali <mehrozali912@gmail.com>
2025-12-27 11:56:28 +0500

    Updating the SignUp for image Uploading

--- Backend/app.js
+++ Backend/app.js
@@ -1,8 +1,15 @@
 const express = require("express");
 const ErrorHandler = require("./utils/ErrorHandler");
 const app = express();
-const cookieParser = require("cookie-parser")
+const cookieParser = require("cookie-parser");
+const bodyParser = require("body-parser");
+const cors = require("cors");
 
+app.use(cors());
+app.use(express.json());
+app.use(cookieParser());
+app.use("/", express.static("uploads"));
+app.use(bodyParser.urlencoded({ extended: true }));
 
 
 
@@ -10,6 +17,13 @@ const cookieParser = require("cookie-parser")
 if (process.env.NODE_ENV !== "PRODUCTION") {
     require("dotenv").config({ path: "backend/config/.env" })
 }
+
+
+
+// import routes 
+const user = require("./controller/user");
+app.use("/api/v2/user", user);
+
 
 
 // its for ErrorHandling 
