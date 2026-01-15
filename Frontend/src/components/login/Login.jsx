3440bed3f27dad59d0968d399c9f32750d09b6ed
Mehrozz-Ali <mehrozali912@gmail.com>
2026-01-16 00:30:32 +0500

    Login user Successful

--- Frontend/src/components/login/Login.jsx
+++ Frontend/src/components/login/Login.jsx
@@ -1,13 +1,32 @@
 import React from 'react'
 import { useState } from 'react'
 import { AiOutlineEye, AiOutlineEyeInvisible } from 'react-icons/ai'
-import { Link } from 'react-router-dom'
+import { Link, useNavigate } from 'react-router-dom'
 import styles from '../../styles/styles'
+import axios from 'axios'
+import server from '../../server'
+import { toast } from 'react-toastify'
 
 function Login() {
     const [email, setEmail] = useState("")
     const [password, setPassword] = useState("")
     const [visible, setVisible] = useState(false)
+    const navigate = useNavigate();
+
+    const handleSubmit = async (e) => {
+        e.preventDefault();
+        await axios.post(`${server}/user/login-user`, {
+            email,
+            password,
+        }).then((res) => {
+            toast.success("Login Successful!");
+            navigate("/");
+        }).catch((err) => {
+            toast.error(err.response.data.message);
+        })
+    }
+
+
     return (
         <div className='min-h-screen bg-gray-50 flex-col justify-center py-12 sm:px-6 lg:px-8'>
             <div className='sm:mx-auto sm:w-full sm:max-w-md'>
@@ -15,7 +34,7 @@ function Login() {
             </div>
             <div className='mt-8 sm:mx-auto sm:w-full sm:max-w-md'>
                 <div className="bg-white py-8 px-4 shadow sm:rounded-lg sm:px-10">
-                    <form className='space-y-6'>
+                    <form className='space-y-6' onSubmit={handleSubmit}>
                         <div>
                             <label htmlFor="email" className='block text-sm font-medium text-gray-700'>Email address</label>
                             <div className="mt-1">
