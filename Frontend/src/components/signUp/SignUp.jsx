ef080c53c986cfc115e345e85a1ac6769b111e07
Mehrozz-Ali <mehrozali912@gmail.com>
2026-01-15 13:44:44 +0500

    User Login updated

--- Frontend/src/components/signUp/SignUp.jsx
+++ Frontend/src/components/signUp/SignUp.jsx
@@ -1,7 +1,7 @@
 import { useState } from 'react'
 import { AiOutlineEye, AiOutlineEyeInvisible } from 'react-icons/ai'
 import { RxAvatar } from 'react-icons/rx'
-import { Link } from 'react-router-dom'
+import { Link, useNavigate } from 'react-router-dom'
 import styles from '../../styles/styles'
 import axios from 'axios'
 import server from '../../server'
@@ -13,6 +13,7 @@ function SignUp() {
     const [password, setPassword] = useState("")
     const [visible, setVisible] = useState(false)
     const [avatar, setAvatar] = useState(null)
+    const navigate = useNavigate();
 
 
 
@@ -33,7 +34,9 @@ function SignUp() {
         newForm.append("email", email);
         newForm.append("password", password);
         axios.post(`${server}/user/create-user`, newForm, config).then((res) => {
-            console.log(res);
+            if(res.data.success===true){
+                navigate("/");
+            }
         }).catch((err) => {
             console.log(err);
         })
