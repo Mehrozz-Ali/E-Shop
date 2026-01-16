506017f922bf0411c2c137739152728340eee558
Mehrozz-Ali <mehrozali912@gmail.com>
2026-01-16 22:28:21 +0500

    dropdown item updated

--- Frontend/src/components/Layout/Header.jsx
+++ Frontend/src/components/Layout/Header.jsx
@@ -4,11 +4,17 @@ import { Link } from 'react-router-dom';
 import { useState } from 'react';
 import { productData } from '../../static/data';
 import { AiOutlineSearch } from 'react-icons/ai';
+import { IoIosArrowDown, IoIosArrowForward } from 'react-icons/io';
+import { BiMenuAltLeft } from 'react-icons/bi';
+import DropDown from './DropDown';
+import { categoriesData } from '../../static/data';
 
 
 function Header() {
     const [searchTerm, setSearchTerm] = useState("");
     const [searchData, setSearchData] = useState(null);
+    const [active, setActive] = useState(false);
+    const [dropDown, setDropDown] = useState(false);
 
 
     const handleSearchChange = (e) => {
@@ -19,6 +25,15 @@ function Header() {
         );
         setSearchData(filteredProducts);
     }
+
+    window.addEventListener("scroll", () => {
+        if (window.screenY > 70) {
+            setActive(true);
+        } else {
+            setActive(false);
+        }
+    })
+
     return (
         <>
             <div className={`${styles.section}`}>
@@ -29,6 +44,7 @@ function Header() {
                         </Link>
                     </div>
 
+                    {/* Search Box  */}
                     <div className="w-[50%] relative">
                         <input type="text" placeholder="Search Product..." value={searchTerm} onChange={handleSearchChange} className="h-[40px] w-full px-2 border-[#3957db] border-[2px] rounded-md" />
                         <AiOutlineSearch size={30} className="absolute right-2 top-1.5 cursor-pointer" />
@@ -48,6 +64,27 @@ function Header() {
                                 })}
                             </div>
                         ) : null}
+                    </div>
+
+                    <div className={`${styles.button}`}>
+                        <Link to="/seller">
+                            <h1 className='text-[#fff] flex items-center'>Become a Seller<IoIosArrowForward className="ml-1" /></h1>
+                        </Link>
+                    </div>
+                </div>
+            </div>
+
+            <div className={`${active === true ? "shadow-sm fixed top-0 left-0 z-10" : null} transition  800px:flex items-center justify-between w-full bg-[#3321c8] h-[70px]`}>
+                <div className={`${styles.section} relative ${styles.normalFlex} justify-between`}>
+                    {/* Categories */}
+                    <div>
+                        <div className='relative h-[60px] mt-[10px] w-[270px]  1000px:block'>
+                            <BiMenuAltLeft size={30} className='absolute top-3 left-2' />
+                            <button className={`h-[100%] w-full flex  justify-between items-center pl-10 bg-white font-sans text-lg font-[500] select-none rounded-t-md`}>All Categories</button>
+                            <IoIosArrowDown size={20} className='absolute right-2 top-4 cursor-pointer'  onClick={()=>setDropDown(!dropDown)}/>
+                                { dropDown ?
+                                (<DropDown categoriesData={categoriesData} setDropDown={setDropDown}/>):null}
+                        </div>
                     </div>
                 </div>
             </div>
