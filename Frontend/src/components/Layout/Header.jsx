dba251b2ae051739e779538eba3b9e83ef83736b
Mehrozz-Ali <mehrozali912@gmail.com>
2026-01-16 18:14:07 +0500

    search filter updated

--- Frontend/src/components/Layout/Header.jsx
+++ Frontend/src/components/Layout/Header.jsx
@@ -14,9 +14,9 @@ function Header() {
     const handleSearchChange = (e) => {
         const term = e.target.value;
         setSearchTerm(term);
-        const filteredProducts = productData && productData.filter((product) => {
+        const filteredProducts = productData && productData.filter((product) =>
             product.name.toLocaleLowerCase().includes(term.toLocaleLowerCase())
-        });
+        );
         setSearchData(filteredProducts);
     }
     return (
@@ -34,12 +34,17 @@ function Header() {
                         <AiOutlineSearch size={30} className="absolute right-2 top-1.5 cursor-pointer" />
                         {searchData && searchData.length !== 0 ? (
                             <div className='absolute min-h-[30vh] bg-slate-50 shadow-sm-2 z-[9] p-4'>
-                                {searchData && searchData.map((item, index) => {
+                                {searchData && searchData.map((i, index) => {
                                     const d = i.name;
                                     const product_name = d.replace(/\s+/g, "-");
-                                    return (<Link to={`/product/${product_name}`}>
-
-                                    </Link>)
+                                    return (
+                                        <Link to={`/product/${product_name}`}>
+                                            <div className="w-full flex items-start-py-3">
+                                                <img src={i.image_Url[0].url} alt="" className='w-[40px] h-[40px] mr-[10px]' />
+                                                <h1>{i.name}</h1>
+                                            </div>
+                                        </Link>
+                                    )
                                 })}
                             </div>
                         ) : null}
