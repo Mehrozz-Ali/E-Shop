edb5179f55c467ca0f0bf0a00ee0b9467178fe17
Mehrozz-Ali <mehrozali912@gmail.com>
2026-01-17 00:58:23 +0500

    Hero Section

--- Frontend/src/components/Layout/Navbar.jsx
+++ Frontend/src/components/Layout/Navbar.jsx
@@ -9,8 +9,9 @@ function Navbar({ active }) {
             {
                 navItems && navItems.map((i, index) => (
                     <div className='flex'>
-                        <Link to={i.url} className={`${active === index + 1 ? "text-[#17dd1f]" : "text-[#fff]"} font-[500] px-6 cursor-pointer`} />
-                        {i.title}
+                        <Link to={i.url} className={`${active === index + 1 ? "text-[#17dd1f]" : "text-[#fff]"} font-[500] px-6 cursor-pointer`} >
+                            {i.title}
+                        </Link>
                     </div>
                 ))
             }
--- Frontend/src/components/home/Home.jsx
+++ Frontend/src/components/home/Home.jsx
@@ -1,10 +1,12 @@
 import React from 'react'
 import Header from '../Layout/Header';
+import Hero from '../Route/Hero/Hero'
 
 function Home() {
   return (
     <div>
-      <Header activeHeading={1}/>
+      <Header activeHeading={1} />
+      <Hero />
     </div>
   )
 }
