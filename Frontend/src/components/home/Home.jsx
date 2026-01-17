7f07b9f9b290676ab4f831e892804ddee32732c3
Mehrozz-Ali <mehrozali912@gmail.com>
2026-01-17 13:02:08 +0500

    categories page is done

--- Frontend/src/components/home/Home.jsx
+++ Frontend/src/components/home/Home.jsx
@@ -1,12 +1,14 @@
 import React from 'react'
 import Header from '../Layout/Header';
-import Hero from '../Route/Hero/Hero'
+import Hero from '../Route/Hero/Hero';
+import Categories from '../Route/Categories/Categories';
 
 function Home() {
   return (
     <div>
       <Header activeHeading={1} />
       <Hero />
+      <Categories />
     </div>
   )
 }
