1e229684de353ee8cf82996db83ae84d6c84df45
Mehrozz-Ali <mehrozali912@gmail.com>
2026-01-18 20:36:18 +0500

    Footer updated

--- Frontend/src/components/Route/Events/EventsCard.jsx
+++ Frontend/src/components/Route/Events/EventsCard.jsx
@@ -4,14 +4,14 @@ import CountDown from "./CountDown.jsx"
 
 function EventsCard() {
     return (
-        <div className={`w-full block bg-white rounded-lg lg:flex p-2 shadow-sm `}>
+        <div className={`w-full block bg-white rounded-lg lg:flex mb-2 shadow-sm `}>
             <div className='w-full lg:w-[50%] m-auto'>
                 <img src="https://m.media-amazon.com/images/I/31Vle5fVdaL.jpg" alt="" />
             </div>
 
-            <div className="w-full lg:w-[50%] flex flex-col justify-center">
+            <div className="w-full lg:w-[50%] flex flex-col justify-center ">
                 <h2 className={`${styles.productTitle}`}>Iphone 14pro Max B/256gb</h2>
-                <p className='text-justify'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quo possimus odit voluptate autem blanditiis. Optio, eaque repudiandae maiores aperiam at sequi adipisci esse deleniti incidunt illum, vitae voluptate cupiditate accusamus!
+                <p className=''>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quo possimus odit voluptate autem blanditiis. Optio, eaque repudiandae maiores aperiam at sequi adipisci esse deleniti incidunt illum, vitae voluptate cupiditate accusamus!
                     Qui accusamus alias ipsa magni error esse ullam! Possimus suscipit omnis eveniet, nisi ut natus. Necessitatibus esse ut dolor dignissimos dicta iste sint possimus optio. Iste aut laborum enim commodi.
                 </p>
                 <div className="flex py-2 justify-between">
