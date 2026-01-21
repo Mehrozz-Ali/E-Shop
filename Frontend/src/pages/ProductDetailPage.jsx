49050d3ce0cf61e6befe253dded83227037c637d
Mehrozz-Ali <mehrozali912@gmail.com>
2026-01-21 23:41:55 +0500

    ProductDetails Code updated

--- Frontend/src/pages/ProductDetailPage.jsx
+++ Frontend/src/pages/ProductDetailPage.jsx
@@ -1,9 +1,31 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
+import Header from '../components/Layout/Header';
+import ProductDetail from '../components/Products/ProductDetail';
+import Footer from '../components/Layout/Footer';
+import { useParams } from 'react-router-dom';
+import { productData } from '../static/data';
 
 function ProductDetailPage() {
-  return (
-    <div>ProductDetailPage</div>
-  )
+    const { name } = useParams();
+    const [data, setData] = useState(null);
+    const productName = name.replace(/-/g, " ");
+
+    console.log(name);
+
+
+    useEffect(() => {
+        const data = productData.find((i) => i.name === productName);
+        setData(data);
+    }, [])
+
+
+    return (
+        <div>
+            <Header />
+            <ProductDetail data={data} />
+            <Footer />
+        </div>
+    )
 }
 
 export default ProductDetailPage
