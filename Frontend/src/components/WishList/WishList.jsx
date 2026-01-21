6b9e46181dc9db93ed59de93153e4b4d90783346
Mehrozz-Ali <mehrozali912@gmail.com>
2026-01-21 17:26:31 +0500

    wish list code updated

--- Frontend/src/components/WishList/WishList.jsx
+++ Frontend/src/components/WishList/WishList.jsx
@@ -1,9 +1,9 @@
 import { RxCross1 } from 'react-icons/rx';
-import { IoBagHandleOutline } from 'react-icons/io5';
 import styles from '../../styles/styles';
 import { useState } from 'react';
-import { HiOutlineMinus, HiPlus } from 'react-icons/hi';
-import { Link } from 'react-router-dom'
+import { Link } from 'react-router-dom';
+import { BsCartPlus } from 'react-icons/bs';
+import { AiOutlineHeart } from 'react-icons/ai';
 
 function WishList({ setOpenWishList }) {
     const cartData = [
@@ -32,7 +32,7 @@ function WishList({ setOpenWishList }) {
                     </div>
                     {/* Items length  */}
                     <div className={`${styles.normalFlex} p-4`}>
-                        <IoBagHandleOutline size={25} />
+                        <AiOutlineHeart size={25} />
                         <h5 className='pl-2 text-[20px] font-[500]'>3 items </h5>
                     </div>
 
@@ -44,14 +44,6 @@ function WishList({ setOpenWishList }) {
                         ))}
                     </div>
                 </div>
-                <div className="px-5 mb-3 ">
-                    {/* CHeckOut BUtton */}
-                    <Link to="/checkout">
-                        <div className={`h-[45px] flex items-center justify-center w-[100%] bg-[#e44343] rounded-[5px]`}>
-                            <h1 className='text-[#fff] text-[18px]'>Checkout Now (USD$1080)</h1>
-                        </div>
-                    </Link>
-                </div>
             </div>
         </div>
     )
@@ -62,24 +54,20 @@ const CartSingle = ({ data }) => {
     const totalPrice = data.price * value;
 
     return (
-        <div className="border-b p-2 ">
+        <div className="border-b p-4 ">
             <div className="w-full flex items-center">
-                <div>
-                    <div className={`bg-[#e44343] border border-[#e4434373] rounded-full w-[25px] h-[25px] ${styles.normalFlex} justify-center cursor-pointer`} onClick={() => setValue(value + 1)}>
-                        <HiPlus size={18} color="#fff" />
-                    </div>
-                    <span className='pl-[10px]'>{value}</span>
-                    <div className="bg-[#a7abb14f] rounded-full w-[25px] h-[25px] flex items-center justify-center cursor-pointer" onClick={() => setValue(value === 1 ? 1 : value - 1)}>
-                        <HiOutlineMinus size={16} color="#7d879c" />
-                    </div>
-                </div>
+                <RxCross1 className='cursor-pointer' />
                 <img src="https://bonik-react.vercel.app/assets/images/products/Fashion/Clothes/1.SilverHighNeckSweater.png" alt="image" className='w-[80px] h-[80px] ml-2' />
+
                 <div className='pl-[5px]'>
                     <h1>{data.name}</h1>
                     <h4 className='font-[400] text-[15px] text-[#00000082]'>${data.price}*{value}</h4>
                     <h4 className='font-[600] text-[17px] pt-[3px] text-[#d02222] font-Roboto'>US${totalPrice}</h4>
                 </div>
-                <RxCross1 className='cursor-pointer' />
+
+                <div>
+                    <BsCartPlus size={20} className="cursor-pointer" title="Add to cart" />
+                </div>
             </div>
         </div>
     )
