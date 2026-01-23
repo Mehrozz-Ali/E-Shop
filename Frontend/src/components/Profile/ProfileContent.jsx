91e04e2af8686a9316b5c448108a39ab1a9ea205
Mehrozz-Ali <mehrozali912@gmail.com>
2026-01-23 23:16:40 +0500

    ProfileSidebar & ProfilePage code done

---
+++ Frontend/src/components/Profile/ProfileContent.jsx
@@ -0,0 +1,21 @@
+import React from 'react';
+import { useSelector } from 'react-redux';
+import { backend_url } from '../../server';
+
+
+function ProfileContent({ active }) {
+    const { user } = useSelector((state) => state.user);
+    return (
+        <div className='w-full'>
+            {active === 1 && (
+                <div className='flex justify-center w-full '>
+                    <div className="relative">
+                        <img src={`${backend_url}${user.avatar.url}`} alt="" className="w-[35px] h-[35px] rounded-full" />
+                    </div>
+                </div>
+            )}
+        </div>
+    )
+}
+
+export default ProfileContent
--- Frontend/src/pages/ProfilePage.jsx
+++ Frontend/src/pages/ProfilePage.jsx
@@ -1,8 +1,23 @@
-import React from 'react'
+import React, { useState } from 'react'
+import Headers from '../components/Layout/Header';
+import styles from '../styles/styles';
+import ProfileSidebar from '../components/Profile/ProfileSidebar';
+import ProfileContent from '../components/Profile/ProfileContent';
+
+
 
 function ProfilePage() {
+  const [active, setActive] = useState(1);
   return (
-    <div>ProfilePage</div>
+    <div>
+      <Headers />
+      <div className={`${styles.section} flex bg-[#f5f5f5] py-10`}>
+        <div className='w-[335px]'>
+          <ProfileSidebar active={active} setActive={setActive} />
+        </div>
+        <ProfileContent active={active}/>
+      </div>
+    </div>
   )
 }
 
