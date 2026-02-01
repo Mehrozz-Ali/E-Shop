7ecb2a7f449581477413b8cc178e43b898cc2b31
Mehrozz-Ali <mehrozali912@gmail.com>
2026-02-01 23:53:41 +0500

    Shop Headers code done

--- Frontend/src/pages/Shop/ShopDashboardPage.jsx
+++ Frontend/src/pages/Shop/ShopDashboardPage.jsx
@@ -1,9 +1,18 @@
 import React from 'react'
+import DashboardHeader from '../../components/Shop/Layout/DashboardHeader'
+import DashboardSidebar from '../../components/Shop/Layout/DashboardSidebar';
 
 function ShopDashboardPage() {
-  return (
-    <div>ShopDashboardPage</div>
-  )
+    return (
+        <div>
+            <DashboardHeader />
+            <div className="flex items-center justify-between w-full">
+                <div className='w-[80px] md:w-[300px]'>
+                    <DashboardSidebar active={1} />
+                </div>
+            </div>
+        </div>
+    )
 }
 
 export default ShopDashboardPage
