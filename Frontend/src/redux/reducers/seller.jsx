002d1c23e336b91bbeb0bc520e64afc7bd4b6363
Mehrozz-Ali <mehrozali912@gmail.com>
2026-02-01 15:08:38 +0500

    Shop HOme page bug fix

--- Frontend/src/redux/reducers/seller.jsx
+++ Frontend/src/redux/reducers/seller.jsx
@@ -1,7 +1,8 @@
 import { createReducer } from "@reduxjs/toolkit";
 
 const initialState = {
-    isAuthenticated: false,
+    isSeller: false,
+    seller: null,
 };
 
 export const sellerReducer = createReducer(initialState, (builder) => {
@@ -10,14 +11,14 @@ export const sellerReducer = createReducer(initialState, (builder) => {
             state.isLoading = true;
         })
         .addCase("LoadSellerSuccess", (state, action) => {
-            state.isAuthenticated = true;
+            state.isSeller = true;
             state.isLoading = false;
             state.seller = action.payload;
         })
         .addCase("LoadSellerFail", (state, action) => {
             state.isLoading = false;
             state.error = action.payload;
-            state.isAuthenticated = false;
+            state.isSeller = false;
         })
         .addCase("clearErrors", (state) => {
             state.error = null;
