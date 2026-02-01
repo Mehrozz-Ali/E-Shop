8e8f2d1fb786bf68e9bfd25313925ef3fb97c8a9
Mehrozz-Ali <mehrozali912@gmail.com>
2026-02-01 23:53:41 +0500

    Shop Headers code done

---
+++ Frontend/src/components/Layout/Loader.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import Lottie from 'react-lottie';
+import animationData from '../../Assets/Animation/Shopping Cart Loader.json'
+
+function Loader() {
+    const defaultOptions
+  return (
+    <div>Loader</div>
+  )
+}
+
+export default Loader
