fab1324c5345141703edd2cd54e338fd2b6698fc
Mehrozz-Ali <mehrozali912@gmail.com>
2026-02-02 13:01:51 +0500

    ShopInfo & ShopHomePage Done

--- Frontend/src/components/Layout/Loader.jsx
+++ Frontend/src/components/Layout/Loader.jsx
@@ -3,10 +3,19 @@ import Lottie from 'react-lottie';
 import animationData from '../../Assets/Animation/Shopping Cart Loader.json'
 
 function Loader() {
-    const defaultOptions
-  return (
-    <div>Loader</div>
-  )
+    const defaultOptions = {
+        loop: false,
+        autoplay: true,
+        animationData: animationData,
+        renderSettings: {
+            preserveAspectRatio: 'xMidYMid slice'
+        }
+    }
+    return (
+        <div className='w-full h-screen flex items-center justify-center'>
+            <Lottie options={defaultOptions} height={300} width={300} />
+        </div>
+    )
 }
 
 export default Loader
