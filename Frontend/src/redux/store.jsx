e9b3bb763bea2d9cf91f299cdfae471b8721f7b9
Mehrozz-Ali <mehrozali912@gmail.com>
2026-02-06 12:20:33 +0500

    Product code done successfully

--- Frontend/src/redux/store.jsx
+++ Frontend/src/redux/store.jsx
@@ -1,12 +1,15 @@
 import { configureStore } from "@reduxjs/toolkit";
 import { userReducer } from "./reducers/user.jsx";
 import { sellerReducer } from "./reducers/seller.jsx";
+import { productReducer } from "./reducers/product.jsx";
 
 
 const Store = configureStore({
     reducer: {
         user: userReducer,
         seller: sellerReducer,
+        products: productReducer,
+
     },
 })
 
