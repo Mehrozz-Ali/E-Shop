7cdfe081a0e959232cf77e0d521d8e822f0daf02
Mehrozz-Ali <mehrozali912@gmail.com>
2026-02-07 19:50:21 +0500

    All products done successfully

--- Frontend/src/redux/actions/product.jsx
+++ Frontend/src/redux/actions/product.jsx
@@ -26,7 +26,7 @@ export const createProduct = (newForm) => async (dispatch) => {
 
 
 // get all products of a shop
-export const getAllProductsShop = () => async (dispatch) => {
+export const getAllProductsShop = (id) => async (dispatch) => {
     try {
         dispatch({
             type: "getAllProductsShopRequest"
@@ -39,6 +39,25 @@ export const getAllProductsShop = () => async (dispatch) => {
     } catch (error) {
         dispatch({
             type: "getAllProductsShopFailed",
+            payload: error.response.data.message,
+        })
+    }
+}
+
+// delete product of a shop
+export const deleteProduct = (id) => async (dispatch) => {
+    try {
+        dispatch({
+            type: "deleteProductRequest",
+        })
+        const { data } = await axios.delete(`${server}/product/delete-shop-product/${id}`, { withCredentials: true });
+        dispatch({
+            type: "deleteProductSuccess",
+            payload: data.message,
+        })
+    } catch (error) {
+        dispatch({
+            type: "deleteProductFailed",
             payload: error.response.data.message,
         })
     }
