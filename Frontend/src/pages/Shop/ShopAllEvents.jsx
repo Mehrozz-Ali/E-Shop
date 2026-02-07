794d52905f7d6fe6a3659c91c7ae1b8e3dbe4f57
Mehrozz-Ali <mehrozali912@gmail.com>
2026-02-08 00:35:19 +0500

    All-Events code Done

--- Frontend/src/pages/Shop/ShopAllEvents.jsx
+++ Frontend/src/pages/Shop/ShopAllEvents.jsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import DashboardHeader from '../../components/Shop/Layout/DashboardHeader'
 import DashboardSidebar from '../../components/Shop/Layout/DashboardSidebar'
-import AllEvents from '../../components/Shop/AllEvents.jsx';
+import AllEvents from '../../components/Shop/AllEvents';
 
 function ShopAllEvents() {
     return (
--- Frontend/src/redux/actions/event.jsx
+++ Frontend/src/redux/actions/event.jsx
@@ -20,4 +20,43 @@ export const createEvent = (newForm) => async (dispatch) => {
             payload: error.response.data.message,
         })
     }
+};
+
+// get all events of a shop
+export const getAllEventsShop = (id) => async (dispatch) => {
+    try {
+        dispatch({
+            type: "getAllEventsShopRequest"
+        })
+        const { data } = await axios.get(`${server}/event/get-all-events/${id}`);
+        dispatch({
+            type: "getAllEventsShopSuccess",
+            payload: data.events,
+        })
+    } catch (error) {
+        dispatch({
+            type: "getAllEventsShopFailed",
+            payload: error.response.data.message,
+        })
+    }
+}
+
+
+// delete event of a shop
+export const deleteEvent = (id) => async (dispatch) => {
+    try {
+        dispatch({
+            type: "deleteEventRequest",
+        })
+        const { data } = await axios.delete(`${server}/event/delete-shop-event/${id}`, { withCredentials: true });
+        dispatch({
+            type: "deleteEventSuccess",
+            payload: data.message,
+        })
+    } catch (error) {
+        dispatch({
+            type: "deleteEventFailed",
+            payload: error.response.data.message,
+        })
+    }
 }
