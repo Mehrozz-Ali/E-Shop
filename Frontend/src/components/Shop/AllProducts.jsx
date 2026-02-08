04f94da6e1a351179f3d91780744ebe4ad2da9e7
Mehrozz-Ali <mehrozali912@gmail.com>
2026-02-08 14:34:15 +0500

    Coupon Code creation in process

--- Frontend/src/components/Shop/AllProducts.jsx
+++ Frontend/src/components/Shop/AllProducts.jsx
@@ -1,5 +1,5 @@
 import { DataGrid } from '@mui/x-data-grid';
-import React, { useEffect } from 'react'
+import React, { useEffect, useState } from 'react'
 import { AiOutlineDelete, AiOutlineEye } from 'react-icons/ai';
 import { useDispatch, useSelector } from 'react-redux';
 import { deleteProduct, getAllProductsShop } from '../../redux/actions/product';
--- Frontend/src/pages/Shop/ShopAllCoupons.jsx
+++ Frontend/src/pages/Shop/ShopAllCoupons.jsx
@@ -1,8 +1,24 @@
 import React from 'react'
+import DashboardHeader from '../../components/Shop/Layout/DashboardHeader'
+import DashboardSidebar from '../../components/Shop/Layout/DashboardSidebar'
+import AllCoupons from '../../components/Shop/AllCoupons';
+
+
 
 function ShopAllCoupons() {
   return (
-    <div>ShopAllCoupons</div>
+    <div>
+      <DashboardHeader />
+      <div className="flex justify-between w-full">
+        <div className='w-[80px] md:w-[300px]'>
+          <DashboardSidebar active={9} />
+        </div>
+
+        <div className="w-full justify-center flex">
+          <AllCoupons />
+        </div>
+      </div>
+    </div>
   )
 }
 
