741fce14ca777e6cb3b36bf6f7f80a17f56f9ddf
Mehrozz-Ali <mehrozali912@gmail.com>
2026-02-12 15:55:45 +0500

    Productcard bug fixed

--- Frontend/src/components/Route/BestDeals/BestDeals.jsx
+++ Frontend/src/components/Route/BestDeals/BestDeals.jsx
@@ -8,24 +8,22 @@ import { getAllProducts } from '../../../redux/actions/product';
 import axios from 'axios';
 
 function BestDeals() {
-    
-     const [products, setProducts] = useState([]);
+
+    const [products, setProducts] = useState([]);
 
     // Fetch all products from all shops
     useEffect(() => {
         axios.get("http://localhost:8000/api/v2/product/get-all-products")
             .then(res => {
-                // Sort by sold_out descending and take top 10
-                const topSold = res.data.products
-                    .sort((a, b) => b.sold_out - a.sold_out)
+                const topPriced = res.data.products
+                    .sort((a, b) => Number(b.originalPrice) - Number(a.originalPrice)) // convert to number
                     .slice(0, 10);
-                setProducts(topSold);
+                setProducts(topPriced);
             })
             .catch(err => console.log(err));
     }, []);
 
-    const sortedProducts = products?.sort((a, b) => b.sold_out - a.sold_out).slice(0, 10);
-
+    const sortedProducts = products;
     return (
         <div>
             <div className={`${styles.section}`}>
@@ -33,11 +31,6 @@ function BestDeals() {
                     <h1>Best Deals</h1>
                 </div>
                 <div className='grid grid-cols-l gap-[20px] md:grid-cols-2 md:gap-[25px] lg:grid-cols-4 lg:gap-[25px] xl:grid-cols-5 xl:gap-[30px] mb-12 border-0 '>
-                    {/* {
-                        data && data.map((i, index) => (
-                            <ProductCard data={i} key={index} />
-                        ))
-                    } */}
                     {sortedProducts?.length > 0 ? (
                         sortedProducts.map((i) => (
                             <ProductCard data={i} key={i._id} />
