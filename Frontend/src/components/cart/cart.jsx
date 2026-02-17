8c81bd62e44a8c0de1a50ddb41d7250b19124540
Mehrozz-Ali <mehrozali912@gmail.com>
2026-02-17 12:52:20 +0500

    add to cart items in process

--- Frontend/src/components/cart/cart.jsx
+++ Frontend/src/components/cart/cart.jsx
@@ -4,25 +4,25 @@ import styles from '../../styles/styles';
 import { useState } from 'react';
 import { HiOutlineMinus, HiPlus } from 'react-icons/hi';
 import { Link } from 'react-router-dom'
+import { backend_url } from '../../server';
+import { useDispatch, useSelector } from 'react-redux';
+import { addToCart, removeFromCart } from '../../redux/actions/cart';
 
 function cart({ setOpenCart }) {
-    const cartData = [
-        {
-            name: "Iphone 14 Pro Max 256 gb and 8gb ram silver color",
-            description: "test",
-            price: 999
-        },
-        {
-            name: "Iphone 14 Pro Max 256 gb and 8gb ram silver color",
-            description: "test",
-            price: 300,
-        },
-        {
-            name: "Iphone 14 Pro Max 256 gb and 8gb ram silver color",
-            description: "test",
-            price: 600,
-        },
-    ]
+    const { cart } = useSelector((state) => state.cart);
+    const dispatch = useDispatch();
+
+    const removeFromCartHandler = () => {
+        dispatch(removeFromCart(data));
+    }
+
+    const totalPrice = cart.reduce((acc, item) => acc + item.qty * item.discountPrice, 0);
+
+
+    const quantityChangeHandler = (data) => {
+        dispatch(addToCart(data));
+    }
+
     return (
         <div className='fixed top-0 left-0 w-full bg-[#0000004b] h-screen z-10'>
             <div className="fixed top-0 right-0 min-h-full w-[25%] bg-white flex flex-col justify-between shadow-sm">
@@ -39,8 +39,8 @@ function cart({ setOpenCart }) {
                     {/* cart single item */}
                     <br />
                     <div className='w-full border-t'>
-                        {cartData && cartData.map((i, index) => (
-                            <CartSingle key={index} data={i} />
+                        {cart && cart.map((i, index) => (
+                            <CartSingle key={index} data={i} quantityChangeHandler={quantityChangeHandler} removeFromCartHandler={removeFromCartHandler} />
                         ))}
                     </div>
                 </div>
@@ -48,7 +48,7 @@ function cart({ setOpenCart }) {
                     {/* CHeckOut BUtton */}
                     <Link to="/checkout">
                         <div className={`h-[45px] flex items-center justify-center w-[100%] bg-[#e44343] rounded-[5px]`}>
-                            <h1 className='text-[#fff] text-[18px]'>Checkout Now (USD$1080)</h1>
+                            <h1 className='text-[#fff] text-[18px]'>Checkout Now (USD${totalPrice})</h1>
                         </div>
                     </Link>
                 </div>
@@ -57,26 +57,38 @@ function cart({ setOpenCart }) {
     )
 }
 
-const CartSingle = ({ data }) => {
-    const [value, setValue] = useState(1);
-    const totalPrice = data.price * value;
+const CartSingle = ({ data, quantityChangeHandler, removeFromCartHandler }) => {
+    const [value, setValue] = useState(data.qty);
+    const totalPrice = data.discountPrice * value;
+
+    const increament = (data) => {
+        setValue(value + 1);
+        const updateCartData = { ...data, qty: value + 1 };
+        quantityChangeHandler(updateCartData);
+    }
+
+    const decrement = (data) => {
+        setValue(value === 1 ? 1 : value - 1);
+        const updateCartData = { ...data, qty: value === 1 ? 1 : value - 1 };
+        quantityChangeHandler(updateCartData);
+    }
 
     return (
         <div className="border-b p-2 ">
             <div className="w-full flex items-center">
                 <div>
-                    <div className={`bg-[#e44343] border border-[#e4434373] rounded-full w-[25px] h-[25px] ${styles.normalFlex} justify-center cursor-pointer`} onClick={() => setValue(value + 1)}>
+                    <div className={`bg-[#e44343] border border-[#e4434373] rounded-full w-[25px] h-[25px] ${styles.normalFlex} justify-center cursor-pointer`} onClick={() => increament(data)}>
                         <HiPlus size={18} color="#fff" />
                     </div>
-                    <span className='pl-[10px]'>{value}</span>
-                    <div className="bg-[#a7abb14f] rounded-full w-[25px] h-[25px] flex items-center justify-center cursor-pointer" onClick={() => setValue(value === 1 ? 1 : value - 1)}>
+                    <span className='pl-[10px]'>{data.qty}</span>
+                    <div className="bg-[#a7abb14f] rounded-full w-[25px] h-[25px] flex items-center justify-center cursor-pointer" onClick={() => decrement(data)}>
                         <HiOutlineMinus size={16} color="#7d879c" />
                     </div>
                 </div>
-                <img src="https://bonik-react.vercel.app/assets/images/products/Fashion/Clothes/1.SilverHighNeckSweater.png" alt="image" className='w-[80px] h-[80px] ml-2' />
+                <img src={`${backend_url}${data.images[0]}`} alt="image" className='w-[130px] h-min ml-2 mr-2 rounded-[5px]' />
                 <div className='pl-[5px]'>
                     <h1>{data.name}</h1>
-                    <h4 className='font-[400] text-[15px] text-[#00000082]'>${data.price}*{value}</h4>
+                    <h4 className='font-[400] text-[15px] text-[#00000082]'>${data.discountPrice}*{value}</h4>
                     <h4 className='font-[600] text-[17px] pt-[3px] text-[#d02222] font-Roboto'>US${totalPrice}</h4>
                 </div>
                 <RxCross1 className='cursor-pointer' />
