dd6dc29ca2b29f0141cd052ada35a0cbbf9a1a71
Mehrozz-Ali <mehrozali912@gmail.com>
2026-02-28 17:54:23 +0500

    payment method backend code done successfully

---
+++ Backend/controller/payment.js
@@ -0,0 +1,29 @@
+const express = require("express");
+const router = express.Router();
+const stripe = require("stripe")(process.env.STRIPE_SECRET_KEY);
+
+const catchAsyncErrors = require("../middleware/catchAsyncErrors");
+
+
+
+
+router.post("/payment/process", catchAsyncErrors(async (req, res, next) => {
+    const myPayment = await stripe.paymentIntents.create({
+        amount: req.body.amount,
+        currency: "inr",
+        metadata: {
+            company: "Becodemy",
+        },
+    });
+    res.status(201).json({
+        success: true,
+        client_secret: myPayment.client_secret,
+    })
+}))
+
+
+router.get("/stripeapikey", catchAsyncErrors(async (req, res, next) => {
+    res.status(200).json({
+        stripeApiKey: process.env.STRIPE_API_KEY,
+    })
+}))
