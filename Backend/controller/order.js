a5292c9eda106aed9414e23ad0437955a49e8a32
Mehrozz-Ali <mehrozali912@gmail.com>
2026-03-16 21:20:21 +0500

    making controller for order

--- Backend/controller/order.js
+++ Backend/controller/order.js
@@ -2,10 +2,21 @@ const express = require("express");
 const router = express.Router();
 const ErrorHandler = require("../utils/ErrorHandler");
 const catchAsyncErrors = require("../middleware/catchAsyncErrors");
-const {isAuthenticated} = require("../middleware/auth");
+const { isAuthenticated } = require("../middleware/auth");
 const Order = require("../model/order");
-const Product=require("../model/product");
+const Product = require("../model/product");
 
 
 // create new order
+router.post("/create-order", isAuthenticated, catchAsyncErrors(async (req, res, next) => {
+    try {
+        const { cart, shippingAddress, user, totaPrice, paymentInfo } = req.body;
+        // group cart items by shopId
+
+
+    } catch (error) {
+        return next(new ErrorHandler(error.message, 400));
+    }
+}))
+
 
