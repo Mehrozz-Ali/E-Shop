be27024f0e879dcb1ea35873f327eca23d7cb88e
Mehrozz-Ali <mehrozali912@gmail.com>
2026-03-24 17:22:42 +0500

    order success page under process

--- Frontend/src/routes/Routes.jsx
+++ Frontend/src/routes/Routes.jsx
@@ -13,6 +13,7 @@ import SellerActivationPage from '../pages/SellerActivationPage';
 import ShopLoginPage from '../pages/ShopLoginPage';
 import CheckoutPage from '../pages/CheckoutPage';
 import PaymentPage from '../pages/PaymentPage'
+import OrderSuccessPage from '../pages/OrderSuccessPage';
 
 export {
     LoginPage,
@@ -30,4 +31,5 @@ export {
     ShopLoginPage,
     CheckoutPage,
     PaymentPage,
+    OrderSuccessPage,
 }
